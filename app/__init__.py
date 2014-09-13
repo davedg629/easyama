f2cf761029000a5bd1437abc7cc1c77738d9eb22
David DiGiovanni <davedg629@gmail.com>
2014-09-13 16:03:47 -0400

    Add praw to init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
 from flask.ext.login import LoginManager
+import praw
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,5 +13,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+
+r = praw.Reddit(user_agent=app.config['REDDIT_USER_AGENT'])
+r.set_oauth_app_info(
+    app.config['REDDIT_APP_ID'],
+    app.config['REDDIT_APP_SECRET'],
+    app.config['OAUTH_REDIRECT_URI']
+)
 
 from app import models, views
